29fc5a2493250a258190e59632c2522fad989777
Volodymyr Nerovnia <nerv@i.ua>
2024-03-08 04:35:23 +0200

    Decomposed class ChartData Created classes: - DiscretChartData - IncreasedChartData

--- web/js/charts.js
+++ web/js/charts.js
@@ -1,7 +1,7 @@
 const ctx1 = document.getElementById('myChart1');
 const ctx2 = document.getElementById('myChart2');
 
-class ChartData {
+class DiscretChartData {
   constructor() {
     this.diffReadMan1 = [];
     this.diffWriteMan1 = [];
@@ -15,89 +15,101 @@ class ChartData {
     this.readObjTimeMan2 = [];
     this.writeMapTimeMan2 = [];
     this.writeObjTimeMan2 = [];
-
-    this.sumDiffReadMan1 = 0;
-    this.sumDiffWriteMan1 = 0;
-    this.sumReadMapTimeMan1 = 0;
-    this.sumReadObjTimeMan1 = 0;
-    this.sumWriteMapTimeMan1 = 0;
-    this.sumWriteObjTimeMan1 = 0;
-    this.sumDiffReadMan2 = 0;
-    this.sumDiffWriteMan2 = 0;
-    this.sumReadMapTimeMan2 = 0;
-    this.sumReadObjTimeMan2 = 0;
-    this.sumWriteMapTimeMan2 = 0;
-    this.sumWriteObjTimeMan2 = 0;
+  }
+}
+class IncreasedChartData {
+  constructor() {
+    this.sumDiffReadMan1 = [];
+    this.sumDiffWriteMan1 = [];
+    this.sumReadMapTimeMan1 = [];
+    this.sumReadObjTimeMan1 = [];
+    this.sumWriteMapTimeMan1 = [];
+    this.sumWriteObjTimeMan1 = [];
+    this.sumDiffReadMan2 = [];
+    this.sumDiffWriteMan2 = [];
+    this.sumReadMapTimeMan2 = [];
+    this.sumReadObjTimeMan2 = [];
+    this.sumWriteMapTimeMan2 = [];
+    this.sumWriteObjTimeMan2 = [];
   }
 }
 
 
-//console.log(measurements);
-const chartData = {
-  ch1: new ChartData(), 
-  ch2: new ChartData(),
-}
+class ChartData {
+  constructor(data, chartSteps) {
+    this.discret = new DiscretChartData();
+    this.increased = new IncreasedChartData();
+    this.#dataApproximation(data, chartSteps);
+    this.axis_x = this.#getAxisX(chartSteps);
+  }
 
-const dataApproximation = (data, chartData) => {
-  console.log(data);
-  const chartSteps = 40;
-  let surrStep = 0; 
-  const oneStep = Math.floor(data.length / chartSteps);
-  for(let i = 0; i < chartSteps; i++) {
-    let diffReadMan1 = 0;
-    let diffWriteMan1 = 0;
-    let readMapTimeMan1 = 0;
-    let readObjTimeMan1 = 0;
-    let writeMapTimeMan1 = 0;
-    let writeObjTimeMan1 = 0;
-
-    let diffReadMan2 = 0;
-    let diffWriteMan2 = 0;
-    let readMapTimeMan2 = 0;
-    let readObjTimeMan2 = 0;
-    let writeMapTimeMan2 = 0;
-    let writeObjTimeMan2 = 0;
-    for(let j = 0; (j < oneStep) || ((i * chartSteps + j) < data.length); j++) {
-      diffReadMan1 += data[i * chartSteps + j].man1.diffRead;
-      diffWriteMan1 += data[i * chartSteps + j].man1.diffWrite;
-      readMapTimeMan1 += data[i * chartSteps + j].man1.readMapTime;
-      readObjTimeMan1 += data[i * chartSteps + j].man1.readObjTime;
-      writeMapTimeMan1 += data[i * chartSteps + j].man1.writeMapTime;
-      writeObjTimeMan1 += data[i * chartSteps + j].man1.writeObjTime;
-
-      diffReadMan2 += data[i * chartSteps + j].man2.diffRead;
-      diffWriteMan2 += data[i * chartSteps + j].man2.diffWrite;
-      readMapTimeMan2 += data[i * chartSteps + j].man2.readMapTime;
-      readObjTimeMan2 += data[i * chartSteps + j].man2.readObjTime;
-      writeMapTimeMan2 += data[i * chartSteps + j].man2.writeMapTime;
-      writeObjTimeMan2 += data[i * chartSteps + j].man2.writeObjTime;
+  #getAxisX(chartSteps) {
+    const x = [];
+    for(let i = 0; i< chartSteps; i++) {
+      x.push(i.toString(10));
     }
+    return x;
+  }
 
-    chartData.ch1.diffReadMan1.push(diffReadMan1 / chartSteps);
-    chartData.ch1.diffWriteMan1.push(diffWriteMan1 / chartSteps);
-    chartData.ch1.readMapTimeMan1.push(readMapTimeMan1 / chartSteps);
-    chartData.ch1.readObjTimeMan1.push(readObjTimeMan1 / chartSteps);
-    chartData.ch1.writeMapTimeMan1.push(writeMapTimeMan1 / chartSteps);
-    chartData.ch1.writeObjTimeMan1.push(writeObjTimeMan1 / chartSteps);
-
-    chartData.ch1.diffReadMan2.push(diffReadMan2 / chartSteps);
-    chartData.ch1.diffWriteMan2.push(diffWriteMan2 / chartSteps);
-    chartData.ch1.readMapTimeMan2.push(readMapTimeMan2 / chartSteps);
-    chartData.ch1.readObjTimeMan2.push(readObjTimeMan2 / chartSteps);
-    chartData.ch1.writeMapTimeMan2.push(writeMapTimeMan2 / chartSteps);
-    chartData.ch1.writeObjTimeMan2.push(writeObjTimeMan2 / chartSteps);
+  #dataApproximation(data, chartSteps) {
+    console.log(data);
+    console.log('------------------');
+    console.log(Object.keys(data[0]?.man1));
+    console.log('------------------');
+    let surrStep = 0; 
+    const oneStep = Math.floor(data.length / chartSteps);
+    for(let i = 0; i < chartSteps; i++) {
+      let diffReadMan1 = 0;
+      let diffWriteMan1 = 0;
+      let readMapTimeMan1 = 0;
+      let readObjTimeMan1 = 0;
+      let writeMapTimeMan1 = 0;
+      let writeObjTimeMan1 = 0;
+  
+      let diffReadMan2 = 0;
+      let diffWriteMan2 = 0;
+      let readMapTimeMan2 = 0;
+      let readObjTimeMan2 = 0;
+      let writeMapTimeMan2 = 0;
+      let writeObjTimeMan2 = 0;
+      for(let j = 0; (j < oneStep) || ((i * chartSteps + j) < data.length); j++) {
+        diffReadMan1 += data[i * chartSteps + j].man1.diffRead;
+        diffWriteMan1 += data[i * chartSteps + j].man1.diffWrite;
+        readMapTimeMan1 += data[i * chartSteps + j].man1.readMapTime;
+        readObjTimeMan1 += data[i * chartSteps + j].man1.readObjTime;
+        writeMapTimeMan1 += data[i * chartSteps + j].man1.writeMapTime;
+        writeObjTimeMan1 += data[i * chartSteps + j].man1.writeObjTime;
+  
+        diffReadMan2 += data[i * chartSteps + j].man2.diffRead;
+        diffWriteMan2 += data[i * chartSteps + j].man2.diffWrite;
+        readMapTimeMan2 += data[i * chartSteps + j].man2.readMapTime;
+        readObjTimeMan2 += data[i * chartSteps + j].man2.readObjTime;
+        writeMapTimeMan2 += data[i * chartSteps + j].man2.writeMapTime;
+        writeObjTimeMan2 += data[i * chartSteps + j].man2.writeObjTime;
+      }
+  
+      this.discret.diffReadMan1.push(diffReadMan1 / chartSteps);
+      this.discret.diffWriteMan1.push(diffWriteMan1 / chartSteps);
+      this.discret.readMapTimeMan1.push(readMapTimeMan1 / chartSteps);
+      this.discret.readObjTimeMan1.push(readObjTimeMan1 / chartSteps);
+      this.discret.writeMapTimeMan1.push(writeMapTimeMan1 / chartSteps);
+      this.discret.writeObjTimeMan1.push(writeObjTimeMan1 / chartSteps);
+  
+      this.discret.diffReadMan2.push(diffReadMan2 / chartSteps);
+      this.discret.diffWriteMan2.push(diffWriteMan2 / chartSteps);
+      this.discret.readMapTimeMan2.push(readMapTimeMan2 / chartSteps);
+      this.discret.readObjTimeMan2.push(readObjTimeMan2 / chartSteps);
+      this.discret.writeMapTimeMan2.push(writeMapTimeMan2 / chartSteps);
+      this.discret.writeObjTimeMan2.push(writeObjTimeMan2 / chartSteps);
+    }
   }
 }
 
+const chartSteps = 40;
+const chartData = new ChartData(measurements, chartSteps);
+console.log(chartData);
 
-const approximatedMeasurements = dataApproximation(measurements, chartData);
-
-
-const x = [];
-for(let i=0; i< measurements.length; i++) {
-  x.push(i.toString(10));
-}
-
+/*
 for(const el of measurements) {
 
   chartData.ch2.diffReadMan1.push(el.man1.diffRead);
@@ -143,8 +155,8 @@ for(const el of measurements) {
   chartData.ch1.writeObjTimeMan2.push(chartData.ch1.sumWriteObjTimeMan2);
   
 }
-
-
+*/
+/*
 new Chart(ctx1, {
   type: 'line',
   data: {
@@ -189,41 +201,44 @@ new Chart(ctx1, {
       }
     }
   }
-});
+});*/
+
+console.log(chartData.discret)
+
 new Chart(ctx2, {
   type: 'line',
   data: {
-    labels: x,
+    labels: chartData.axis_x,
     datasets: [
       {
       label: 'diffReadMan1',
-      data: chartData.ch2.diffReadMan1,
+      data: chartData.discret.diffReadMan1,
       borderWidth: 1
     },
     {
       label: 'diffWriteMan1',
-      data: chartData.ch2.diffWriteMan1,
+      data: chartData.discret.diffWriteMan1,
       borderWidth: 1
     },
     {
       label: 'readMapTimeMan1',
-      data: chartData.ch2.readMapTimeMan1,
+      data: chartData.discret.readMapTimeMan1,
       borderWidth: 1
     },
     {
       label: 'readObjTimeMan1',
-      data: chartData.ch2.readObjTimeMan1,
+      data: chartData.discret.readObjTimeMan1,
       borderWidth: 1
     },
 
     {
       label: 'writeMapTimeMan1',
-      data: chartData.ch2.writeMapTimeMan1,
+      data: chartData.discret.writeMapTimeMan1,
       borderWidth: 1
     },
     {
       label: 'writeObjTimeMan1',
-      data: chartData.ch2.writeObjTimeMan1,
+      data: chartData.discret.writeObjTimeMan1,
       borderWidth: 1
     },
   ]
