a70ef660dbb15533a3d577e95d693e6caf7812b5
Volodymyr Nerovnia <nerv@i.ua>
2024-03-17 23:41:08 +0200

    Added configuration

---
+++ src/config/app.js
@@ -0,0 +1,13 @@
+module.exports = {
+  minElementsInstances: 100,
+  maxElementsInstances: 1000000,
+  stepElementsInstances: 10,
+  
+  maxArrLength: 10000,
+  maxValueFigure: 5000,
+  get maxTestIterations() { 
+    return Math.floor(this.maxArrLength * 1.5)
+  },
+  
+  numberOfMeasurements: 5000,
+}
