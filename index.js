3d70d96b5fda44ac9159de50d69339805e8db234
Volodymyr Nerovnia <nerv@i.ua>
2024-03-20 02:03:59 +0200

    Enhanced index,js

--- index.js
+++ index.js
@@ -6,38 +6,18 @@
 
 const performanceTest = require('./src/test.js');
 const saveMeasurements = require('./src/saveMeasurements.js');
+const { 
+  maxArrLength, 
+  maxTestIterations, 
+  maxValueFigure, 
+  numberOfMeasurements 
+} = require('./src/config/app.js');
+const { testDataFile } = require('./src/config/app.js');
 
-const testDataDirectory = './data';
-const testDataFileName = 'node-test-time-obj-map.js';
-
-const minElementsInstances = 100;
-const maxElementsInstances = 1000000;
-const stepElementsInstances = 10;
-
-const maxArrLength = 10000;
-const maxValueFigure = 5000;
-const maxTestIterations = Math.floor(maxArrLength * 1.5);
-
-const numberOfMeasurements = 5000;
 const arrMeasurements = [];
-
-
 
 for (let i = 0; i < numberOfMeasurements; i++) {
   arrMeasurements.push(performanceTest(maxArrLength, maxTestIterations, maxValueFigure));
 }
 
-
-saveMeasurements(JSON.stringify(arrMeasurements), testDataDirectory, testDataFileName);
-//console.log();
-
-/*
-console.log(`Write time for Objects = ${writeObjTime} Maps = ${writeMapTime}`);
-console.log(`Read time for Objects = ${readObjTime} Maps = ${readMapTime}`);
-console.log(`Total time for Objects = ${totalObjTime} Maps = ${totalMapTime}`);
-*/
-/*
-console.time('Test')
-
-console.timeEnd('Test')
-*/
+saveMeasurements(JSON.stringify(arrMeasurements), testDataFile.directory, testDataFile.name);
